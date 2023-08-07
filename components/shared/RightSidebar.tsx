deba9c56cf2ff808c6a8e998c9558a8b0c2b2044
frbarbre <fr.barbre@gmail.com>
2023-08-07 21:02:00 +0200

    second commit

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,13 @@
 export default function RightSidebar() {
     return (
-      <div>
-        <h1>RightSidebar</h1>
-      </div>
+      <section className="custom-scrollbar rightsidebar">
+        <div className="flex flex-1 flex-col justify-start">
+          <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+        </div>
+        <div className="flex flex-1 flex-col justify-start">
+          <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+        </div>
+      </section>
     );
   }
   
